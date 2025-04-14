cf9353faf3208ca178e94832f638adf0ced6a96d
nasra <naasironasra@gmail.com>
2025-04-14 18:01:11 +0300

    initial commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,33 @@
+// // App.jsx
+// import React, {useState} from "react";
+
+// const App = () => {
+//     const [expenses, setExpenses] = useState([]);
+//     const [search, setSearch] = useState("");
+//     const [sortBy, setSortBy] = useState("");
+
+//     const handleAddExpense = (newExpense) => {
+//         setExpenses([...expenses, {newExpense, id: Date.now()}]);
+//     };
+
+//     const filteredExpenses = expenses.filter(
+//         (expense) =>
+//             expense.name.toLowerCase().includes(search.toLowerCase()) ||
+//             expenses.description.toLowerCase().includes(search.toLowerCase())
+//     );
+
+//     const sortedExpenses = [...filteredExpenses].sort((a,b) => {
+//         if (!sortBy) return 0;
+//         return a[sortBy].localeCompare(b[sortBy]);
+//     });
+
+//     return (
+//         <div className="p-6 max-w-4xl mx-auto space-y-6">
+//             <ExpenseForm onAddExpense={handleAddExpense}/>
+//             <SearchAndSort setSearch={setSearch} setSortBy={setSortBy}/>
+//             <ExpenseTable expenses={sortedExpenses} onDelete={handleDelete} />
+//         </div>
+//     );
+// };
+
+// export default App;
